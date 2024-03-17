9a25006fa38b751a946b2f4af5e450fc08e7fbc4
RyanRibeiroM <ryanribero998@gmail.com>
2024-03-17 13:11:59 -0300

    modificações Rian

--- js/perfil.js
+++ js/perfil.js
@@ -1,8 +1,23 @@
+const imgUsuario = document.getElementById("imgUsuario");
+const menuUsuario = document.getElementById("menuUsuario");
+let controle = 0;
 function sair() {
   const resposta = window.confirm(
     "VOCÊ TEM CERTEZA QUE DESEJA SAIR DA PAGINA?"
   );
   if (resposta) {
-    window.location.href = "login.html";
+    var newWindow = window.open("login.html", "_blank");
+    newWindow.focus(); // Foca na nova janela
+    window.close(); // Fecha a janela anterior
+  }
+}
+
+function mostrarMenuUsuario() {
+  if (controle === 0) {
+    menuUsuario.style.display = "block";
+    controle = 1;
+  } else {
+    menuUsuario.style.display = "none";
+    controle = 0;
   }
 }
