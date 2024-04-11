0014c412b81aa55d62d2bc094dcded320d0a4cc5
Welington Sousa <wp.sousa.2004@gmail.com>
2024-04-11 00:08:43 -0300

    restartando

--- js/main.js
+++
@@ -1,7 +0,0 @@
-function selecionarbotao(elemento) {
-  console.log("entrou");
-  const botoes = document.querySelectorAll("#botoes button");
-  botoes.forEach((button) => button.classList.remove("selecionado"));
-
-  elemento.classList.add("selecionado");
-}
--- js/perfil2.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('edit-profile').addEventListener('click', function() {
-    alert('Editar perfil');
-    // Adicione aqui a lógica para editar o perfil, como abrir um formulário de edição
-  });
