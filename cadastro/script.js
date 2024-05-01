cb0ca7a6becaa862a4e8963c50122e6ec99afdd8
Welington Sousa <wp.sousa.2004@gmail.com>
2024-05-01 14:52:59 -0300

    alteração tela cadastro pela décima vez

--- cadastro/script.js
+++ cadastro/script.js
@@ -12,6 +12,9 @@ window.onload = function () {
     }
 };
 
+
+
+
 const container = document.getElementById('container');
 const registrarBtn = document.getElementById('registrar');
 const loginBtn = document.getElementById('login');
